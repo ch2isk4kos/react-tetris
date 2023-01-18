4e994c46dee597c47f16270c562d35dbecccaae5
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-18 10:47:31 -0500

    add component for start button

---
+++ client/src/components/StartButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StartButton = ({ callback }) => {
+  return (
+    <div className="Start-Button">
+      <h1>Start Game</h1>
+    </div>
+  );
+};
+
+export default StartButton;
