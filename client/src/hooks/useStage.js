24081c157be669c38337a7fa3db04ff8c89926bc
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-19 14:58:36 -0500

    fix: change naming convention

--- client/src/hooks/useStage.js
+++ client/src/hooks/useStage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { createStage } from "../gameHelpers";
 
-export const usePlayer = () => {
+export const useStage = () => {
   const [stage, setStage] = useState(createStage());
   const [rowsCleared, setRowsCleared] = useState(0);
 
