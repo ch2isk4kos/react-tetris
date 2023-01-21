d3647f0928f543ecfa1cd97d9b9d9461e95e8d62
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-21 12:37:28 -0500

    create functionality to check for wall collisions

--- client/src/gameHelpers.js
+++ client/src/gameHelpers.js
@@ -6,3 +6,25 @@ export const createStage = () => {
     new Array(STAGE_WIDTH).fill([0, "clear"])
   );
 };
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  //
+  for (let y = 0; y < player.tetromino.length; y++) {
+    for (let x = 0; x < player.tetromino[y].length; x++) {
+      // 1. check for a tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+          // 2. check if move is out of bounds in height (y)
+          !stage[y + player.position.y + moveY] ||
+          // 3. check if move is out of bounds in width (x)
+          !stage[x + player.position.x + moveX] ||
+          // 4. check that cell isn't set to "clear"
+          stage[y + player.position.y + moveY][
+            x + player.position.x + moveX
+          ][1] !== "clear"
+        )
+          return true;
+      }
+    }
+  }
+};
