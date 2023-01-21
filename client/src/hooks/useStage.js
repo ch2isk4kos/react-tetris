dc2586b6f1218fca1f11b94d722b3b6af9ac5654
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-20 19:17:05 -0500

    add useEffect && assign variable to clear stage from previous render

--- client/src/hooks/useStage.js
+++ client/src/hooks/useStage.js
@@ -1,9 +1,20 @@
 import { useState, useEffect } from "react";
 import { createStage } from "../gameHelpers";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
   const [rowsCleared, setRowsCleared] = useState(0);
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // clear stage from previous render
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+    };
+
+    setStage((prev) => updateStage(prev));
+  }, []);
 
   return [stage, setStage, rowsCleared];
 };
