ff6f5af7b07a0ebb7fc957b514952408c8e6e23f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-21 12:50:21 -0500

    remove comment(s)

--- client/src/gameHelpers.js
+++ client/src/gameHelpers.js
@@ -8,7 +8,6 @@ export const createStage = () => {
 };
 
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
-  //
   for (let y = 0; y < player.tetromino.length; y++) {
     for (let x = 0; x < player.tetromino[y].length; x++) {
       // 1. check for a tetromino cell
