12d6374caa3611473369608229930a3fc00791fe
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-24 15:19:07 -0500

    add comment(s)

--- client/src/hooks/useStage.js
+++ client/src/hooks/useStage.js
@@ -21,12 +21,12 @@ export const useStage = (player, resetPlayer) => {
     };
 
     const updateStage = (prevStage) => {
-      // clear stage from previous render
+      // 1. clear stage from previous render
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
       );
 
-      // draw the tetromino
+      // 2. draw the tetromino
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0 || value === "undefined") {
@@ -38,7 +38,7 @@ export const useStage = (player, resetPlayer) => {
         });
       });
 
-      // check for collision
+      // 3. check for collision
       if (player.isCollided) {
         resetPlayer();
         return sweep(newStage);
